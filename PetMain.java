0b13d2f92ddf16cbafced742a4b1135b5744828f
結月ゆかり <yonemori550@yahoo.co.jp>
2025-05-12 06:00:56 +0900

    first commit

---
+++ PetMain.java
@@ -0,0 +1,17 @@
+// Pet.java
+public class Pet {
+    // フィールド（属性）を定義してください
+    
+    // コンストラクタを定義してください
+    
+    // メソッド（操作）を定義してください
+    
+}
+
+// PetMain.java
+public class PetMain {
+    public static void main(String[] args) {
+        // ここにインスタンス生成と操作のコードを書いてください
+        
+    }
+}
